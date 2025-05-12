98a6cd4c59b4b09fed851a07d1fac2c601fa2b63
Dyesse YUMBA <yumba.nduba@gmail.com>
2025-05-12 15:16:41 +0100

    🎉 feat: add product api

---
+++ backend/src/prisma/migrations/20250512140802_product/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `sellingPrice` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_Product" ("createdAt", "description", "id", "name", "updatedAt") SELECT "createdAt", "description", "id", "name", "updatedAt" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
