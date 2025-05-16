f61ea3236835dbeba32cdbfd04daac10e99062c1
Dyesse YUMBA <yumba.nduba@gmail.com>
2025-05-16 15:30:16 +0100

    🚧 wip: installed react router

---
+++ frontend/src/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router";
+import App from "./App";
+// import Layout from './routes/layout';
+// import Dashboard from './routes/dashboard';
+// import Login from './routes/login';
+
+// export const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Layout />}>
+//       <Route index element={<Dashboard />} />
+//       <Route path="login" element={<Login />} />
+//     </Route>
+//   )
+// );
+
+const router = createBrowserRouter([{ path: "/", Component: App }]);
+
+export { router };
