1d5b162e748b9d0b82fc416f801aba081ef235f4
Dyesse YUMBA <yumba.nduba@gmail.com>
2026-01-11 23:06:11 +0100

    🐛 fix: set host

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,8 @@ import 'dotenv/config';
 
 const start = async () => {
   try {
-    await app.listen({ port: Number(process.env.PORT ?? 4000) });
+    // await app.listen({ port: Number(process.env.PORT ?? 4000) });
+    await app.listen({ port: Number(process.env.PORT ?? 4000), host: '0.0.0.0' });
     app.log.info(`🚀 Server running on localhost: ${Number(process.env.PORT ?? 4000)})}`);
   } catch (err) {
     app.log.error(err);
